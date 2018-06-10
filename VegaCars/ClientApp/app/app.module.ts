c852438c86a36cecd85aa676cc2ec65e7b01513b
Erik Butcher <butcer0@gmail.com>
2018-06-10 16:25:35 -0400

    introduced features

--- VegaCars/ClientApp/app/app.module.ts
+++ VegaCars/ClientApp/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { RouterModule } from '@angular/router';
+
 
 import { AppComponent } from './components/app/app.component';
 import { NavMenuComponent } from './components/navmenu/navmenu.component';
 import { HomeComponent } from './components/home/home.component';
 import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
 import { CounterComponent } from './components/counter/counter.component';
+import { VehicleFormComponent } from './components/vehicle-form/vehicle-form.component';
+import { VehicleService } from './services/vehicle.service';
 
 @NgModule({
     declarations: [
@@ -16,7 +20,8 @@ import { CounterComponent } from './components/counter/counter.component';
         NavMenuComponent,
         CounterComponent,
         FetchDataComponent,
-        HomeComponent
+        HomeComponent,
+        VehicleFormComponent
     ],
     imports: [
         CommonModule,
@@ -27,8 +32,12 @@ import { CounterComponent } from './components/counter/counter.component';
             { path: 'home', component: HomeComponent },
             { path: 'counter', component: CounterComponent },
             { path: 'fetch-data', component: FetchDataComponent },
+            { path: 'vehicles/new', component: VehicleFormComponent },
             { path: '**', redirectTo: 'home' }
-        ])
+        ]),
+    ],
+    providers: [
+        VehicleService
     ]
 })
 export class AppModuleShared {
