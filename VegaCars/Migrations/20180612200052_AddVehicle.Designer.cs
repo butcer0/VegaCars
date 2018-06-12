d38a2bf2bfd65264b328f91fa6df50fd17cdc81a
Erik Butcher <butcer0@gmail.com>
2018-06-12 16:03:39 -0400

    Introduced Vehicles and VehicleFeatures mapping table to SQLServer

--- VegaCars/Migrations/20180612200052_AddVehicle.Designer.cs
+++ VegaCars/Migrations/20180612200052_AddVehicle.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using VegaCars.Persistence;
 
 namespace VegaCars.Migrations
 {
     [DbContext(typeof(VegaDbContext))]
-    partial class VegaDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180612200052_AddVehicle")]
+    partial class AddVehicle
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- VegaCars/Persistence/VegaDbContext.cs
+++ VegaCars/Persistence/VegaDbContext.cs
@@ -11,10 +11,18 @@ namespace VegaCars.Persistence
     {
         public DbSet<Make> Makes { get; set; }
         public DbSet<Feature> Features { get; set; }
+
+
+
         public VegaDbContext(DbContextOptions<VegaDbContext> options)
             :base(options)
         {
 
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<VehicleFeature>().HasKey(vf =>   new { vf.VehicleId, vf.FeatureId });
         }
     }
 }
