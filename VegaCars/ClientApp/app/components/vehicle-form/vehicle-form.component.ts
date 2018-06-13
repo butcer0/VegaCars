fb302cc3db473d6f2a85810e1b7ebd30f4aede2c
Erik Butcher <butcer0@gmail.com>
2018-06-13 01:34:18 -0400

    Updated vehicle-form model

--- VegaCars/ClientApp/app/components/vehicle-form/vehicle-form.component.ts
+++ VegaCars/ClientApp/app/components/vehicle-form/vehicle-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { VehicleService } from '../../services/vehicle.service';
+import { Event } from '@angular/router/src/events';
 
 @Component({
   selector: 'app-vehicle-form',
@@ -9,7 +10,10 @@ import { VehicleService } from '../../services/vehicle.service';
 export class VehicleFormComponent implements OnInit {
     makes: any[] = [];
     models: any[] = [];
-    vehicle: any = {};
+    vehicle: any = {
+        features: [],
+        contact: {}
+    };
     features: any[] = [];
 
     constructor(private vehicleService: VehicleService) { }
@@ -21,10 +25,19 @@ export class VehicleFormComponent implements OnInit {
     }
 
     onMakeChange() {
-        var selectedMake = this.makes.find(m => m.id == this.vehicle.make);
+        var selectedMake = this.makes.find(m => m.id == this.vehicle.makeId);
         this.models = selectedMake ? selectedMake.models : [];
-        console.log("VEHICLE", this.vehicle);
+        //console.log("VEHICLE", this.vehicle);
+        delete this.vehicle.modelId;
     }
 
-
+    onFeatureToggle(featureId: number, $event: any) {
+        if ($event.target.checked) {
+            this.vehicle.features.push(featureId);
+        } else {
+            var index = this.vehicle.features.indexOf(featureId);
+            this.vehicle.features.splice(index, 1);
+        }
+            
+    }
 }
