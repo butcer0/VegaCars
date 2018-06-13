65aef7dc542c3ac4cbeb0e74c4ebbb290bdccd33
Erik Butcher <butcer0@gmail.com>
2018-06-13 14:52:18 -0400

    Introduced dynamic table header generation

--- VegaCars/ClientApp/app/components/vehicle-list/vehicle-list.component.ts
+++ VegaCars/ClientApp/app/components/vehicle-list/vehicle-list.component.ts
@@ -12,7 +12,13 @@ export class VehicleListComponent implements OnInit {
     vehicles: Vehicle[];
     makes: KeyValuePair[];
     query: any = {};
-
+    columns = [
+        { title: 'Id' },
+        { title: 'Contact Name', key: 'contactName', isSortable: true },
+        { title: 'Make', key: 'make', isSortable: true },
+        { title: 'Model', key: 'model', isSortable: true },
+        { title: '', key: 'id', isSortable: false },
+    ];
     constructor(private vehicleService: VehicleService) { }
 
     ngOnInit() {
@@ -37,7 +43,7 @@ export class VehicleListComponent implements OnInit {
 
     sortBy(columnName: string) {
         if (this.query.sortBy === columnName) {
-            this.query.isSortAscending = false;
+            this.query.isSortAscending = !this.query.isSortAscending;
 
         } else {
             this.query.sortBy = columnName;
